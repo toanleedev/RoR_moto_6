9c6d0dbbbaaa831b1cff1d628a4177f5b3610b6f
Toanlee <leminhtoan1@dtu.edu.vn>
2022-10-09 14:30:05 +0700

    admin confirm user paper

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -2,12 +2,12 @@ module Admin
   module UsersHelper
     def user_info_fields(user)
       user.attributes.slice('email', 'first_name', 'last_name', 'phone', 'birth',
-                            'gender', 'is_partner', 'is_admin', 'created_at')
+                            'gender', 'is_partner', 'is_admin', 'created_at').compact
     end
 
     def user_paper_infos(paper)
       paper.slice('card_number', 'card_front_url', 'card_back_url',
-                  'driver_number', 'driver_front_url')
+                  'driver_number', 'driver_front_url').compact
     end
   end
 end
